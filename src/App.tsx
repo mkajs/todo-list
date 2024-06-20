a8559fedf5393e239eebdf40652ecd99c161a332
Mahdi <kazemi.dist@gmail.com>
2024-06-20 11:36:52 +0330

    feat: :sparkles: Add overall functionality using reducer, context and custom hook.
    add a function called todoprovider to provide functions to compotents via context.

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import NewtodoForm from "./NewtodoForm";
 import TodoList from "./TodoList";
+import TodoProvider from "./TodoProvider";
 
 const App = () => {
   return (
-    <div className=" mx-auto max-w-96 py-6">
-      <TodoList />
-      <NewtodoForm classname="mt-6" />
-    </div>
+    <TodoProvider>
+      <div className=" mx-auto max-w-96 py-6">
+        <TodoList />
+        <NewtodoForm classname="mt-6" />
+      </div>
+    </TodoProvider>
   );
 };
 export default App;
